961f5fac9288f6fd7e8bce56329a965acb9b14e3
sveneschlbeck <sven.eschlbeck@t-online.de>
2021-08-07 19:28:45 +0200

    changing setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 import codecs
 import os
+from os import path
 
-VERSION = '0.0.1'
+# Reading the contents of README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+	long_description = f.read()
+
+VERSION = '0.1.0'
 DESCRIPTION = 'Converting units'
 
 # Setting up
@@ -12,6 +18,8 @@ setup(
     author="Sven Eschlbeck",
     author_email="<sven.eschlbeck@t-online.de>",
     description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[],
     keywords=['python', 'units', 'scientific', 'metric', 'physics', 'siunits', 'converting', 'unitconverter'],
